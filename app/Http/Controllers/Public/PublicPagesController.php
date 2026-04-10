e4a16e4b431b9953daf65d816d4b99ffde6bb5b5
Lain <allainjamesdayagdag29@gmail.com>
2026-04-10 17:07:24 +0800

    merge branch fixed

--- app/Http/Controllers/Public/PublicPagesController.php
+++ app/Http/Controllers/Public/PublicPagesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
 use App\Models\Scholarship;
-use App\Models\Scholarship;
 use Illuminate\Http\Request;
 
 class PublicPagesController extends Controller
